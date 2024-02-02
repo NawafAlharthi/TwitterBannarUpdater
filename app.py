a2c8a8403f6acb11db3bf3104e25cbf929b865b7
CEONawaf <89227278+NawafAlharthi@users.noreply.github.com>
2024-02-02 03:04:32 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from flask_apscheduler import APScheduler
 import os
 import tweepy
 from PIL import Image
@@ -7,6 +8,7 @@ import pytz
 import time
 
 app = Flask(__name__)
+scheduler = APScheduler()
 
 def update_twitter_header(api, image_paths, current_hour):
     image_path = image_paths[current_hour - 1] 
@@ -43,5 +45,13 @@ def update_header():
     
     return "Twitter header updated!"
 
+# Schedule the function to run every minute
+@scheduler.task('interval', id='update_header_task', seconds=60)
+def scheduled_update_header():
+    with app.app_context():
+        update_header()
+
 if __name__ == '__main__':
+    scheduler.init_app(app)
+    scheduler.start()
     app.run(debug=True)
